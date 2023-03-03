068211f03b9c2eb4ecda6118eeb94571cd663f96
Stanislav Dyagilev <su-27sm1@rambler.ru>
2023-03-03 18:51:38 +0300

    Add fileURLToPath for deploying to Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
