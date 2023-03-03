30347a2098e73ffb5d23ce46e866024262f7ae8f
Stanislav Dyagilev <su-27sm1@rambler.ru>
2023-03-03 19:47:44 +0300

    Correction webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
+/* eslint-disable import/no-import-module-exports */
+/* eslint-disable import/newline-after-import */
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-export default {
+import path from 'path';
+module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
   devServer: {
     open: true,
     host: 'localhost',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
   ],
+  module: {
+    rules: [
+      // {
+      //     test: /\.(js|jsx)$/i,
+      //     loader: 'babel-loader',
+      // },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
 };
