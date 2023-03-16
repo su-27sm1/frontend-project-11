cfb6ea435c20184ed6994b9521fe646ab2cd16bf
Stanislav Dyagilev <su-27sm1@rambler.ru>
2023-03-16 11:10:01 +0300

    Modify src/locales/index.js
    Creation ru.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,3 +1,4 @@
-import en from './en.js';
+import ru from './ru';
+import en from './en';
 
-export default { en };
+export default { ru, en };
---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    feeds: 'Фиды',
+    posts: 'Посты',
+    preview: 'Просмотр',
+    rssAdded: 'RSS успешно загружен',
+    errors: {
+      rssAlreadyExists: 'RSS уже существует',
+      notValidURL: 'Ссылка должна быть с валидным URL',
+      dataIsNotValid: 'Введенные данные невалидны',
+      noRSS: 'Ресурс не содержит валидный RSS',
+      NetworkError: 'Ошибка сети',
+      defaultError: 'Неизвестная ошибка. Что-то пошло не так',
+    },
+  },
+};
